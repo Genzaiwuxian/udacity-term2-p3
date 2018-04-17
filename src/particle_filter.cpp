ca63071baafa526a7571e6269db2536a12b91121
Genzaiwuxian <31791192+Genzaiwuxian@users.noreply.github.com>
2018-04-17 23:05:50 +0800

    cout
    delete cout diff_x and diff_y

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -225,8 +225,8 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
 					double diff_y = observations_transform[m].y - predictions[n].y;
 					double diff_y2 = diff_y * diff_y;
-					cout << "diff_x2 " << diff_x2 << endl;
-					cout << "diff_y2 " << diff_y2 << endl;
+					// cout << "diff_x2 " << diff_x2 << endl;
+					// cout << "diff_y2 " << diff_y2 << endl;
 
 					double gauss_norm = (1 / (2 * M_PI*std_x*std_y));
 					double exponent = ((diff_x2) / (2 * std_x2) + (diff_y2) / (2 * std_y2));
