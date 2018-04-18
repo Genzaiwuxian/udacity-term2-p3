baa9f70853255db78f10b242b526f20acd62e6f6
Genzaiwuxian <31791192+Genzaiwuxian@users.noreply.github.com>
2018-04-18 14:01:49 +0800

    num=100

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -27,7 +27,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	if (!is_initialized)
 	{
 		//set total particles number
-		num_particles = 50;
+		num_particles = 100;
 
 		default_random_engine gen;
 
