4766823430afead4d310ce1946b75c906f7e745e
Genzaiwuxian <31791192+Genzaiwuxian@users.noreply.github.com>
2018-05-01 18:09:27 +0800

    1

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -28,7 +28,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	if (!is_initialized)
 	{
 		//set total particles number
-		num_particles = 50;
+		num_particles = 100;
 
 		normal_distribution<double> dist_x(x, std[0]);
 		normal_distribution<double> dist_y(y, std[1]);
